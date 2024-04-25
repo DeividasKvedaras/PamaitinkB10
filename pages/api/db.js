aa4f2ecb79d3e2ea443bed2e1a3b8111a7cac48d
Matas Dailyda <matas.dailyda@ld.lt>
2024-04-26 00:34:11 +0300

    rename api

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,9 +1,10 @@
-import { db } from '@vercel/postgres';
+import { db } from "@vercel/postgres";
+
 export default async function handler(request, response) {
   const client = await db.connect();
   try {
     await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
-    const names = ['Fiona', 'Lucy'];
+    const names = ["Fiona", "Lucy"];
     await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
   } catch (error) {
     return response.status(500).json({ error });
